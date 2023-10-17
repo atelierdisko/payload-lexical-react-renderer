7ba86ebefca219c547cd3cbbe7752e2a5785d3b7
schaschjan <jan-jakub@atelierdisko.de>
2023-10-17 15:53:41 +0200

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: "React",
+          react: "React",
         },
       },
     },
