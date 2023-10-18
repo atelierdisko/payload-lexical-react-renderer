7c48565a541a63ec3aaa344a860f0b6e0752ff5c
schaschjan <jan-jakub@atelierdisko.de>
2023-10-18 13:54:25 +0200

    remove obsolete files, update vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,21 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/payloadLexicalReactRenderer.tsx"),
-      name: "MyLib",
+      name: "payloadLexicalReactRenderer",
+      formats: ["es", "umd"],
       // the proper extensions will be added
       fileName: "payload-lexical-react-renderer",
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react"],
+      external: ["react", "react-dom"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
